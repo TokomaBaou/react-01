12afd2e7dd3b68832d9a26cde9b306ecc11b20d9
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-08-12 17:34:54 +0900

    propsの追加

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ColorfulMessage from "./compornents/colorfulMessage";
+
+const App = () => {
+  const onClickButton = () => alert();
+
+  return (
+    <>
+      <h1 style={{ color: `red` }}>こんにちは</h1>
+      <ColorfulMessage color = "blue" >いかがお過ごしですか？
+      </ColorfulMessage>
+
+      <ColorfulMessage color = "pink" >元気です！
+      </ColorfulMessage>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+//関数を使ってコンポーネントを表示できる
+
+//Appを画面に反映させる
+ReactDom.render(<App />, document.getElementById("root"));
